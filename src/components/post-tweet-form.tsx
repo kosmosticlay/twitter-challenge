665551c96ff11635c463b778435058f03c859cb8
kosmosticlay <kosmosticlay@gmail.com>
2024-09-24 21:24:47 +0900

    feat: Enable file upload and storage in PostTweetForm

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components";
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
   display: flex;
@@ -81,12 +82,26 @@ export default function PostTweetForm() {
 
     try {
       setIsLoading(true);
-      await addDoc(collection(db, "tweets"), {
+      const doc = await addDoc(collection(db, "tweets"), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName || "Anonymous",
         userId: user.uid,
       });
+
+      // 첨부 파일이 존재할 때
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+
+        await updateDoc(doc, { photo: url });
+      }
+      setTweet("");
+      setFile(null);
     } catch (error) {
       console.log(error);
     } finally {
@@ -97,6 +112,7 @@ export default function PostTweetForm() {
   return (
     <Form onSubmit={handleSubmit}>
       <TextArea
+        required
         rows={5}
         maxLength={150}
         onChange={handleChange}
